3ab2362ccc7ebfacc896e90df0f582de4d305f91
asadhiraj <114976159+asadhiraj@users.noreply.github.com>
2023-06-18 01:04:46 +0500

    settings.py

--- iblogs/settings.py
+++ iblogs/settings.py
@@ -29,6 +29,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+CSRF_TRUSTED_ORIGINS = [
+    'http://127.0.0.1:8000/',
+    'https://web-production-d354.up.railway.app/',
+]
+
 # Application definition
 
 INSTALLED_APPS = [
